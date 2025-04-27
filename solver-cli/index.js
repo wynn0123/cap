403eb112e890e540db121d417d095d0cbe3419fe
Tiago <70700766+tiagorangel1@users.noreply.github.com>
2025-04-27 13:44:13 +0100

    cli: update versions, add total time and improve ui

--- solver-cli/index.js
+++ solver-cli/index.js
@@ -7,15 +7,23 @@ const args = process.argv.slice(2);
 
 (async () => {
   if (args.length === 0) {
-    console.log(`\n${kleur.cyan("@cap.js/cli")} ${kleur.gray(
+    console.log(`${kleur.bold("@cap.js/cli")} ${kleur.gray(
       "cli solver for cap challenges"
-    )}
+    )}${
+      !process.versions?.bun
+        ? `\n\n ${kleur.yellow().bold("warn")}${kleur.dim(
+            ":"
+          )} bun is not installed and recommended`
+        : ""
+    }
 
 Usage:
-  ${kleur.dim("$")} bunx ${kleur.bold("@cap.js/cli")} <challenges>
+  ${kleur.dim("$")} ${kleur.bold().green("bunx @cap.js/cli")} ${kleur.cyan(
+      "<challenges>"
+    )}
 
 Options:
-  <challenges>  The challenges to solve in
+  ${kleur.cyan("<challenges>")}  The challenges to solve in
                 format \`salt:target\``);
     return;
   }
@@ -33,9 +41,15 @@ Options:
     return [salt, target];
   });
 
-  console.log(`\n${kleur.cyan("@cap.js/cli")} ${kleur.dim(`${challenges.length} challenges`)}\n\n\n`);
+  console.log(
+    `${kleur.cyan("@cap.js/cli")} ${kleur.dim(
+      `${challenges.length} challenges`
+    )}\n\n\n`
+  );
 
-  const solutions = await solver(challenges, {
+  const start = performance.now();
+
+  await solver(challenges, {
     onProgress: (status) => {
       process.stdout.moveCursor(0, -2);
       process.stdout.clearScreenDown();
@@ -46,6 +60,11 @@ Options:
     },
   });
 
+  const end = performance.now();
+  
   process.stdout.moveCursor(0, -2);
   process.stdout.clearScreenDown();
+  console.log(
+    kleur.dim(`\nSolved challenges in ${kleur.bold(Math.round(end - start))}ms`)
+  );
 })();
