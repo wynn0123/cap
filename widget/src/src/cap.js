f368a7bcf266127c9dc21693c86a7080f376b8e3
Tiago <70700766+tiagorangel1@users.noreply.github.com>
2025-05-09 21:21:14 +0100

    fix widget

--- widget/src/src/cap.js
+++ widget/src/src/cap.js
@@ -488,7 +488,7 @@
     let initPromise, solve_pow_function;
 
     initPromise = import(
-      CAP_CUSTOM_WASM_URL || "https://cdn.jsdelivr.net/npm/@cap.js/wasm@0.0.3/browser/cap_wasm.min.js"
+      window.CAP_CUSTOM_WASM_URL || "https://cdn.jsdelivr.net/npm/@cap.js/wasm@0.0.3/browser/cap_wasm.min.js"
     )
       .then((wasmModule) => {
         return wasmModule.default().then((instance) => {
